9cd0acaf97e6841bcf1f473e8a53962ba67437a7
Hsincho <99251348+xinzhuohkust@users.noreply.github.com>
2023-07-17 05:18:10 +0800

    Update shanghai_petition.R

--- shanghai_petition.R
+++ shanghai_petition.R
@@ -1,52 +1,5 @@
-library(tidyverse)
-library(rvest)
-library(rio)
-
-page <- "https://xfb.sh.gov.cn/xinfang/yjzj/feedback/feedbacklist?testpara=0&pageNo=1&pagesize=1000" %>% 
-    read_html() 
-
-links <- page %>% 
-    html_nodes("td>a") %>% 
-    html_attr("href") %>% 
-    sprintf("https://xfb.sh.gov.cn/xinfang/yjzj/feedback/%s", .)
-
-table <- page %>% 
-    html_table(header = TRUE) %>% 
-    pluck(1) %>% 
-    add_column(links = links)
-
-get_contents <- possibly(
-    \(link) {
-        contents_page <- link %>%
-            read_html()
-
-        标题 <- contents_page %>%
-            html_node("li>h6") %>%
-            html_text()
-
-        contents <- contents_page %>%
-            html_nodes("li>p") %>%
-            html_text() %>%
-            str_remove_all("\\\r\\\n")
-
-        names(contents) <- c("来信日期", "信件内容", "回复日期", "回复单位", "回复内容")
-
-        result <- contents %>%
-            as_tibble_row() %>%
-            add_column(标题, .before = 1)
-        
-        Sys.sleep(sample(1:2, 1))
-
-        return(result)
-
-    },
-    otherwise = tribble(
-        ~标题, ~来信日期, ~信件内容, ~回复日期, ~回复单位, ~回复内容,
-        "error!", "error!", "error!", "error!", "error!", "error!"
-    )
-)
 
 filelocation <- getwd()
 
-writeLines(filelocation, "filelocation.txt")
+writeLines(filelocation, "filelocation.csv")
 
