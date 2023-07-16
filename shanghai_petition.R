621e443aeee57afc5b9675b5230650667a05e9a2
Hsincho <99251348+xinzhuohkust@users.noreply.github.com>
2023-07-17 03:24:54 +0800

    Update shanghai_petition.R

--- shanghai_petition.R
+++ shanghai_petition.R
@@ -15,6 +15,42 @@ table <- page %>%
     pluck(1) %>% 
     add_column(links = links)
 
+get_contents <- possibly(
+    \(link) {
+        contents_page <- link %>%
+            read_html()
+
+        标题 <- contents_page %>%
+            html_node("li>h6") %>%
+            html_text()
+
+        contents <- contents_page %>%
+            html_nodes("li>p") %>%
+            html_text() %>%
+            str_remove_all("\\\r\\\n")
+
+        names(contents) <- c("来信日期", "信件内容", "回复日期", "回复单位", "回复内容")
+
+        result <- contents %>%
+            as_tibble_row() %>%
+            add_column(标题, .before = 1)
+        
+        Sys.sleep(sample(1:2, 1))
+
+        return(result)
+
+    },
+    otherwise = tribble(
+        ~标题, ~来信日期, ~信件内容, ~回复日期, ~回复单位, ~回复内容,
+        "error!", "error!", "error!", "error!", "error!", "error!"
+    )
+)
+
+table <- table %>% 
+    set_names(c("title", "agency", "date", "links")) %>% 
+    mutate(data = map(links, get_contents, .progress = TRUE)) %>% 
+    unnest(data)
+
 export(
   table, 
   file = sprintf("data/%s_table.csv", Sys.Date()),
